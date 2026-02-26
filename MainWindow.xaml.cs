3c5b98d857a223d8a4fd6f5ff9a77be8f5e37554
Zsolti <113987666+SigMaZsola@users.noreply.github.com>
2026-02-26 12:41:39 +0100

    Nincs kész

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,213 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Kenó
+{
+    public partial class MainWindow : Window
+    {
+        List<NapiKeno> huzasok = new List<NapiKeno>();
+        List<int> tippek = new List<int>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FeltoltSzamGrid();
+            lbHuzasok.SelectionChanged += lbHuzasok_SelectionChanged;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ReadFile();
+        }
+
+        private void ReadFile()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == false) return;
+
+            lbHuzasok.Items.Clear();
+            huzasok.Clear();
+
+            string[] sorok = File.ReadAllLines(ofd.FileName, Encoding.UTF8);
+            sorok = sorok.Skip(1).ToArray();
+
+            int hibasDb = 0;
+
+            foreach (var sor in sorok)
+            {
+                NapiKeno huzas = new NapiKeno(sor);
+                huzasok.Add(huzas);
+                lbHuzasok.Items.Add(sor);
+
+                if (huzas.hibas)
+                    hibasDb++;
+            }
+
+            if (hibasDb > 0)
+            {
+                MessageBox.Show(
+                    $"Összesen {hibasDb} hibás sor volt.",
+                    "Hibás adatok",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+            }
+        }
+
+        private void lbHuzasok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ErdemesSzámolni();
+        }
+
+        private void FeltoltSzamGrid()
+        {
+            grNumberField.Children.Clear();
+
+            int szam = 1;
+            for (int sor = 0; sor < 10; sor++)
+            {
+                for (int oszlop = 0; oszlop < 8; oszlop++)
+                {
+                    Button btn = new Button();
+                    btn.Content = szam.ToString();
+                    btn.Tag = szam;
+                    btn.Margin = new Thickness(2);
+                    btn.Background = Brushes.LightGreen;
+                    btn.Click += SzamGomb_Click;
+
+                    Grid.SetRow(btn, sor);
+                    Grid.SetColumn(btn, oszlop);
+
+                    grNumberField.Children.Add(btn);
+                    szam++;
+                }
+            }
+        }
+
+        private void SzamGomb_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            int szam = (int)btn.Tag;
+
+            if (!tippek.Contains(szam))
+            {
+                if (tippek.Count == 10)
+                {
+                    MessageBox.Show("Már 10 szám van kiválasztva!");
+                    return;
+                }
+
+                tippek.Add(szam);
+                btn.Background = Brushes.Yellow;
+            }
+            else
+            {
+                tippek.Remove(szam);
+                btn.Background = Brushes.LightGreen;
+            }
+
+            FrissitLabel();
+
+            // Ha 10 tipp van, lezárjuk a tét mezőt
+            if (tippek.Count == 10)
+            {
+                tbOsszeg.IsReadOnly = true;  // már nem írható :contentReference[oaicite:2]{index=2}
+            }
+
+            ErdemesSzámolni();
+        }
+
+        private void FrissitLabel()
+        {
+            if (tippek.Count == 0)
+            {
+                lTippCointainer.Content = "Tippek: (még nincs)";
+                return;
+            }
+            lTippCointainer.Content = "Tippek: " + string.Join(", ", tippek.OrderBy(x => x));
+        }
+
+        private void tbOsszeg_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ErdemesSzámolni();
+        }
+
+        private void ErdemesSzámolni()
+        {
+            if (!int.TryParse(tbOsszeg.Text, out int tet)
+                || tet < 200 || tet > 1000)
+            {
+                lNyeremeny.Foreground = Brushes.Red;
+                lNyeremeny.Content = "Tétnek 200–1000 közé kell esnie!";
+                return;
+            }
+
+            if (tippek.Count < 10)
+            {
+                lNyeremeny.Foreground = Brushes.Red;
+                lNyeremeny.Content = "Add meg mind a 10 tippet!";
+                return;
+            }
+
+            if (lbHuzasok.SelectedIndex < 0)
+            {
+                lNyeremeny.Foreground = Brushes.Red;
+                lNyeremeny.Content = "Válassz egy sorsolást!";
+                return;
+            }
+
+            SzamolNyeremeny(lbHuzasok.SelectedIndex);
+        }
+
+        private void SzamolNyeremeny(int index)
+        {
+            int tet = int.Parse(tbOsszeg.Text);
+            var huzott = huzasok[index].huzottSzamok;
+
+            int talalatok = 0;
+            foreach (int tipp in tippek)
+            {
+                if (huzott.Contains(tipp))
+                    talalatok++;
+            }
+
+            string kulcs = $"{tippek.Count}-{talalatok}";
+            Dictionary<string, int> nyeroParok = new Dictionary<string, int>() {
+                {"10-10",1000000}, {"10-9",8000}, {"10-8",350},
+                {"9-9",100000}, {"9-8",1200}, {"9-7",100},
+                {"8-8",20000}, {"8-7",350}, {"8-6",25},
+                {"7-7",5000}, {"7-6",60}, {"7-5",6},
+                {"6-6",500}, {"6-5",20},
+                {"5-5",200}, {"5-4",10}, {"5-3",2},
+                {"4-4",100}, {"4-3",2},
+                {"3-3",15}, {"3-2",1},
+                {"2-2",6},
+                {"1-1",2}
+            };
+            int szorz = nyeroParok.ContainsKey(kulcs) ? nyeroParok[kulcs] : 0;
+            int nyeremeny = tet * szorz;
+
+            if (szorz == 0)
+            {
+                lNyeremeny.Foreground = Brushes.Red;
+                lNyeremeny.Content = $"Találatok: {talalatok}   – Sajnos nem nyertél!";
+            }
+            else
+            {
+                lNyeremeny.Foreground = Brushes.Green;
+                lNyeremeny.Content =
+                    $"Találatok: {talalatok}   Szorzó: {szorz}x  |  Nyeremény: {nyeremeny:N0} Ft";
+            }
+
+            lNyeremeny.Foreground = Brushes.Red;
+            lNyeremeny.Content =
+                $"Találatok: {talalatok}   Szorzó: {szorz}x  |  Nyeremény: {nyeremeny:N0} Ft";
+        }
+    }
+}
---
+++ NapiKeno.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kenó
+{
+    internal class NapiKeno
+    {
+        int ev;
+
+        private int _het;
+        public bool hibas { get; private set; }
+        public int het
+        {
+            get { return _het; }
+            set
+            {
+                if (value < 1) value = 1;
+                if (value > 52) value = 52;
+                _het = value;
+            }
+        }
+
+        private int _nap;
+        public int nap
+        {
+            get { return _nap; }
+            set
+            {
+                if (value < 1) value = 1;
+                if (value > 7) value = 7;
+                _nap = value;
+            }
+        }
+
+        string huzasDatum;
+        public List<int> huzottSzamok { get; private set; } = new List<int>();
+
+        public NapiKeno(string beolvasottSor)
+        {
+            string[] adat = beolvasottSor.Split(';');
+
+            ev = Convert.ToInt32(adat[0]);
+            het = Convert.ToInt32(adat[1]);
+            nap = Convert.ToInt32(adat[2]);
+
+            huzasDatum = adat[0] + "." + adat[1] + "." + adat[2];
+
+            for (int i = 4; i < adat.Length; i++)
+            {
+                huzottSzamok.Add(Convert.ToInt32(adat[i]));
+            }
+
+            if (huzottSzamok.Count != 20)
+            {
+                hibas = true;
+
+      
+                huzottSzamok.Clear();
+                for (int i = 0; i < 20; i++)
+                {
+                    huzottSzamok.Add(0);
+                }
+            }
+        }
+        public int TalalatSzam(List<int> tippek)
+        {
+            int talalatok = 0;
+            foreach (var item in tippek)
+            {
+                if (huzottSzamok.Contains(item))
+                {
+                    talalatok++;
+                }
+            }
+            return talalatok;
+        }
+        public bool Helyes()
+        {
+            return (huzottSzamok.Count() != 20 || huzottSzamok.Distinct().Count() != 20);
+
+                
+        }
+    }
+    
+}
